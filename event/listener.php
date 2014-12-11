60bf40ebfec529388c4977e24cacd04fe4f54782
dmzx <info@dmzx-web.net>
2014-12-11 08:52:16 +0100

    Files
    files

---
+++ event/listener.php
@@ -0,0 +1,40 @@
+<?php
+/**
+*
+* @package phpBB Extension - Loading indicator
+* @copyright (c) 2014 dmzx
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace dmzx\loadingindicator\event;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup' => 'load_language_on_setup',
+		);
+	}
+
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'dmzx/loadingindicator',
+			'lang_set' => 'loading_indicator',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+}
---
+++ styles/prosilver/template/loading_indicator.js
@@ -0,0 +1,21 @@
+/*
+*
+* styles/prosilver/template/loading_indicator.js
+*
+* @package th23_site
+* @author Thorsten Hartmann (www.th23.net)
+* @copyright (c) 2008 by Thorsten Hartmann (www.th23.net)
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @modified by dmzx (c) 2014 (www.dmzx-web.net) 
+*/
+
+window.onbeforeunload = loading_indicator;
+
+function loading_indicator()
+{
+	var l = document.getElementById('loading-indicator');
+	if (l)
+	{
+		l.style.display = 'block';
+	}				
+}
